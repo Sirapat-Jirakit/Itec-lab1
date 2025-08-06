02c1a24d268927c1b8f4eadfc5cd069c6f3973f8
Sirapat-Jirakit <sirapat.jirakit@gmail.com>
2025-08-06 13:42:45 +0700

    update (core) : add themeforest template and implement tabs class

--- itec-lab1/obj/Debug/net9.0/itec-lab1.AssemblyInfo.cs
+++ itec-lab1/obj/Debug/net9.0/itec-lab1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("itec-lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25c053945a43455fbf0dd8562b53496646c6348d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa18ca81393b4b66eb56dabb5acb642c0b0c4a3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("itec-lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("itec-lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
